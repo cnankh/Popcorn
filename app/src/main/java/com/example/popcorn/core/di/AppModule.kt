f980200437f5e70074601dac868789c497972591
Sina <36819672+cnankh@users.noreply.github.com>
2022-03-20 17:37:41 +0330

    update: AppModule

--- app/src/main/java/com/example/popcorn/core/di/AppModule.kt
+++ app/src/main/java/com/example/popcorn/core/di/AppModule.kt
@@ -4,8 +4,10 @@ import android.app.Application
 import android.content.Context
 import dagger.Binds
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import io.reactivex.rxjava3.disposables.CompositeDisposable
 
 @InstallIn(SingletonComponent::class)
 @Module
@@ -13,5 +15,14 @@ abstract class AppModule {
 
     @Binds
     abstract fun bindContext(application: Application): Context
+
+
+    companion object {
+
+        @Provides
+        fun provideCompositeDisposable(): CompositeDisposable {
+            return CompositeDisposable()
+        }
+    }
 
 }
