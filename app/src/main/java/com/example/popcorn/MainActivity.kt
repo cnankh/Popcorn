a5e6b904a0ac04f9c0926259381eb4cf4a0f2445
Sina <36819672+cnankh@users.noreply.github.com>
2022-03-20 14:01:29 +0330

    core: MyApplication

--- app/src/main/java/com/example/popcorn/MainActivity.kt
+++ app/src/main/java/com/example/popcorn/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.popcorn
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import dagger.hilt.android.AndroidEntryPoint
+import retrofit2.Retrofit
+import timber.log.Timber
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
