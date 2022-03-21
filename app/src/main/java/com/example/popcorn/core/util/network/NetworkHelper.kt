52aaa435634a86880915378529e49d158614d505
Sina <36819672+cnankh@users.noreply.github.com>
2022-03-21 18:39:36 +0330

    core: NetworkHelper

---
+++ app/src/main/java/com/example/popcorn/core/util/network/NetworkHelper.kt
@@ -0,0 +1,27 @@
+package com.example.popcorn.core.util.network
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Inject
+
+class NetworkHelper @Inject constructor(@ApplicationContext var context: Context) {
+
+    fun isConnected(): Boolean {
+        var result = false
+        val connectivityManager =
+            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val networkCapabilities = connectivityManager.activeNetwork ?: return false
+        val activeNetwork =
+            connectivityManager.getNetworkCapabilities(networkCapabilities) ?: return false
+        result = when {
+            activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+            activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+            activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
+            else -> false
+        }
+
+        return result
+    }
+}
